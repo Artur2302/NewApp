98e3e67174269ccdf35d643b1ced931fdd80c69d
Artur Stepanyan <programmingwithArt@gmail.com>
2022-09-08 12:51:04 +0400

    Committing my project2

---
+++ src/main/java/com/example/trainingSystem/service/EmailSenderService.java
@@ -0,0 +1,34 @@
+package com.example.trainingSystem.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailSenderService {
+    @Value("${spring.mail.username}")
+    String username;
+
+    private final JavaMailSender mailSender;
+
+    @Autowired
+    public EmailSenderService(JavaMailSender javaMailSender) {
+        this.mailSender = javaMailSender;
+    }
+
+    public void sendEmail(String toEmail,
+                          String subject,
+                          String body){
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(username);
+        message.setTo(toEmail);
+        message.setText(body);
+        message.setSubject(subject);
+
+        mailSender.send(message);
+
+        System.out.println("Mail Sent Successfully");
+    }
+}
